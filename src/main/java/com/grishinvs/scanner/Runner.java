8e936ab4523dbdc5549e4426a8d59913b8b798d7
Гришин Вадим <vsgrishin@tsconsulting.com>
2020-03-04 17:23:46 +0300

    Удален лишний код из main.

--- src/main/java/com/grishinvs/scanner/Runner.java
+++ src/main/java/com/grishinvs/scanner/Runner.java
@@ -1,72 +1,8 @@
 package com.grishinvs.scanner;
 
-import java.util.concurrent.TimeUnit;
-
 public class Runner {
 
-    public static class A {
-
-        synchronized void synchronizedMethodA() {
-            try {
-                System.out.println("Выполняется метод synchronizedMethodA");
-                Thread.sleep(TimeUnit.SECONDS.toMillis(10));
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-
-        synchronized void synchronizedMethodB() {
-            try {
-                System.out.println("Выполняется метод synchronizedMethodB");
-                Thread.sleep(TimeUnit.SECONDS.toMillis(10));
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-
-    }
-
-    public static class ThreadA extends Thread {
-
-        private A targetClass;
-
-        public ThreadA(String name, A a) {
-            super(name);
-            this.targetClass = a;
-        }
-
-        @Override
-        public void run() {
-            System.out.println("Поток " + this.getName() + " вызвал метод synchronizedMethodA");
-            targetClass.synchronizedMethodA();
-            System.out.println("Поток " + this.getName() + " завершился");
-        }
-
-    }
-
-    public static class ThreadB extends Thread {
-
-        private A targetClass;
-
-        public ThreadB(String name, A a) {
-            super(name);
-            this.targetClass = a;
-        }
-
-        @Override
-        public void run() {
-            System.out.println("Поток " + this.getName() + " вызвал метод synchronizedMethodB");
-            targetClass.synchronizedMethodB();
-            System.out.println("Поток " + this.getName() + " завершился");
-        }
-
-    }
-
     public static void main(String[] args) {
-
-         A a = new A();
-        new ThreadA("первый", a).start();
-        new ThreadB("второй", a).start();
 
     }
 
